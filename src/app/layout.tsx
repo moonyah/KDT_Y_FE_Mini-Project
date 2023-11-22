ecfe7d8af0c55cbc4f7fed1cc29f71b15354cbee
moonyah <dltk456@naver.com>
2023-11-22 18:11:38 +0900

    upstream setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,1 @@
-import React from 'react';
 
-import '../styles/global.css';
-
-interface AppLayout {
-  children: React.ReactNode;
-}
-
-const RootLayout = ({ children }: AppLayout) => (
-<<<<<<< HEAD
-  <html lang='en'>
-=======
-  <html lang='ko'>
->>>>>>> 2d210687261cfe0f724bff005ac77430cadacc0d
-    <body className='box-border'>{children}</body>
-  </html>
-);
-
-export default RootLayout;
